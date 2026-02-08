3472be43d86ac12d9a496bae6eb79933bdddd344
Darshak Barad <darshak.barad1212@gmail.com>
2026-02-08 15:40:41 +0530

    added .gitignore

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+export const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`,
+    );
+    console.log(
+      `\n MongoDB Connected! DB Host: ${connectionInstance.connection.host}`,
+    );
+  } catch (err) {
+    console.error("Database Connection Error! ", err);
+    process.exit(1);
+  }
+};
